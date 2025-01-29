47f4803efc077de7ae90ff71884d1a4f7583eb80
JUJJURIMOHITH <126101407+JUJJURIMOHITH@users.noreply.github.com>
2025-01-29 10:59:30 +0530

    Update urls.py

--- myapp/urls.py
+++ myapp/urls.py
@@ -4,6 +4,7 @@ from .views import *
 app_name='myapp'
 
 urlpatterns=[
+    path('',slides,name='slides'),
     path('',bestphones,name='bestsellingphone'),
     path('mobiledata/<int:id>',mobiledataview,name='mobiledata')
 ]
