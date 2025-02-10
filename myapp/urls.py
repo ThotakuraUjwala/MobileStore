bd42a180909b25165d2fe1463fef087b191819bb
ThotakuraUjwala <thotakuraujwala13@gmail.com>
2025-02-10 11:05:36 +0530

    new

--- myapp/urls.py
+++ myapp/urls.py
@@ -8,7 +8,6 @@ urlpatterns = [
     path('', home, name='home'),  # Use the combined home view for both slides and bestphones
     path('mobiledata/<int:id>', mobiledataview, name='mobiledata'),
     path('mobiles/',allmobiles,name='allmobiles'),
-    path('cart/', cart, name='cart'),
     path('mobiles/<str:brand_name>/', brand_view, name='brand_mobiles'),
     path('mobiles/brand/<str:brand_name>/', brand_mobiles_view, name='brand_mobiles'),
     path('laptopdata/<int:id>', lapdataview, name='lapdata'),
@@ -21,6 +20,9 @@ urlpatterns = [
     path('signup/', signup_view, name='signup'),
     path('login/', login_view, name='login'),
     path('logout/', logout_view, name='logout'),
+    #  path('add-to-cart/<int:item_id>/<str:category>/', add_to_cart, name='add_to_cart'),
+    # path('cart/', cart_view, name='cart'),
+    # path('remove-from-cart/<int:item_id>/', remove_from_cart, name='remove_from_cart'),
 
 
 ]
