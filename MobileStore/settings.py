8d1ef355ec799add94b7430d6f314c34496d2660
JUJJURIMOHITH <mohithjujjuri22@gmail.com>
2025-02-20 20:34:21 +0530

    Updated database configurations and made necessary changes

--- MobileStore/settings.py
+++ MobileStore/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'MobileStore.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django_mobile',
+        'USER': 'root',
+        'PASSWORD': '1234',
+        'HOST': 'localhost',  # or your database server IP
+        'PORT': '3306',       # default MySQL port
     }
 }
 
